c83abcb0c74dba6a4a85bde7a53d4a5a5bc9f5ca
C191239 <zsz2007314zsz@sina.com>
2023-11-28 12:49:21 +0800

    sms2 data struct

--- lib/struct2.ts
+++ lib/struct2.ts
@@ -1,6 +1,57 @@
 // Data struct for sms2
 
-export interface loginData2{
-    "username": string,
-    "password": string
+export interface Email2{
+    email:string
+}
+
+export interface FullUser2{
+    username:string,
+    password:string,
+    realName:string,
+    email:string,
+    // student id
+    sid:string,
+    department:string,
+    captcha:string
+}
+
+export interface PasswordPair2{
+    newPassword:string,
+    oldPassword:string
+}
+
+export interface Result2<D=any>{
+    code:number,
+    message:string,
+    data:D,
+    timeStamp:number
+}
+
+export interface SimpleUser2{
+    username: string,
+    password: string
+}
+
+export interface SimpleUserInfo2{
+    token:string,
+    username:string,
+    sid:string,
+    id:number,
+    expiredAt:number
+}
+
+export interface ResetPasswordInfo2{
+    email:string,
+    captcha:string,
+    newPassword:string
+}
+
+export interface SimpleDraft2{
+    title:string,
+    startDate:string,
+    endDate:string,
+    description:string,
+    fileName:string[],
+    // target screen
+    screenId:string
 }
