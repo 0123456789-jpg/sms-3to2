b971e35dd70ba9e54a203a288b648057b53324ef
C191239 <zsz2007314zsz@sina.com>
2023-11-30 17:51:42 +0800

    partial struct3 & 2 transformers

--- lib/struct3.ts
+++ lib/struct3.ts
@@ -4,6 +4,42 @@ export interface CreateAccount3 {
     email: string
 }
 
-export interface VerifyAccount3 {
+export enum House3 {
+    GW = "GeWu",
+    ZZ = "ZhiZhi",
+    CY = "ChengYi",
+    ZX = "ZhengXin",
+    MD = "MingDe",
+    HY = "HongYI",
+    XJ = "XiJing",
+    ZS = "ZhiShan",
+    XM = "XinMin"
+}
 
+export interface VerifyActivateAccount3 {
+    Activate: {
+        email: string,
+        name: string,
+        id: number,
+        phone: number
+        house: House3 | null,
+        organization: string | null,
+        password: string
+    }
+}
+
+export interface VerifyResetPassword3 {
+    ResetPassword: {
+        email: string,
+        password: string
+    }
+}
+
+export interface AbstractVerifyAccount3 {
+    code: number,
+    variant: any
+}
+
+export interface VerifyAccount3<D = VerifyActivateAccount3 | VerifyResetPassword3> extends AbstractVerifyAccount3 {
+    variant: D
 }
--- lib/transformer.ts
+++ lib/transformer.ts
@@ -1,11 +1,44 @@
 import { InternalAxiosRequestConfig } from "axios";
+import { AbstractVerifyAccount3, VerifyAccount3, VerifyActivateAccount3, VerifyResetPassword3 } from "./struct3";
+import { FullUser2, ResetPasswordInfo2 } from "./struct2";
 
-export function transformCreateAccountRequest(config: InternalAxiosRequestConfig):InternalAxiosRequestConfig<any>{
-    config.url="/auth/activateMail";
+export function transformCreateAccountRequest(config: InternalAxiosRequestConfig): InternalAxiosRequestConfig<any> {
+    config.url = "/auth/activateMail";
     return config;
 }
 
-export function transformVerifyAccountRequest(config: InternalAxiosRequestConfig<any>):InternalAxiosRequestConfig<any>{
-    // TODO
+export function transformVerifyAccountRequest(config: InternalAxiosRequestConfig<any>): InternalAxiosRequestConfig<any> {
+    const abstReq: AbstractVerifyAccount3 = config.data;
+    switch (Object.keys(abstReq.variant)[0]) {
+        case "Activate": {
+            const req: VerifyAccount3<VerifyActivateAccount3> = config.data;
+            const transformed: FullUser2 = {
+                username: req.variant.Activate.id.toString(),
+                password: req.variant.Activate.password,
+                realName: req.variant.Activate.name,
+                email: req.variant.Activate.email,
+                sid: req.variant.Activate.id.toString(),
+                // Reserved
+                department: "01",
+                captcha: req.code.toString()
+            };
+            config.url = "/auth/register";
+            config.data = transformed;
+            break;
+        }
+        case "ResetPassword": {
+            const req: VerifyAccount3<VerifyResetPassword3> = config.data;
+            const transformed: ResetPasswordInfo2 = {
+                email: req.variant.ResetPassword.email,
+                captcha: req.code.toString(),
+                newPassword: req.variant.ResetPassword.password
+            };
+            config.url = "auth/resetPassword";
+            config.data = transformed;
+            break;
+        }
+        default:
+            break;
+    }
     return config;
 }
