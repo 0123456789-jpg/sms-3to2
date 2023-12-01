9d6e059639389685302fb790ad23bd74cd448841
C191239 <zsz2007314zsz@sina.com>
2023-12-01 18:46:59 +0800

    remove unused & sync

--- lib/interceptor.ts
+++ lib/interceptor.ts
@@ -2,26 +2,24 @@ import { AxiosInstance, AxiosResponse, InternalAxiosRequestConfig, default as ax
 import { transformCreateAccountRequest } from "./transformer/request";
 
 function listenRequest(request: InternalAxiosRequestConfig<any>): InternalAxiosRequestConfig<any> | Promise<InternalAxiosRequestConfig<any>> {
-    if (request.method?.toLowerCase() === ("post" || "get")) {
-        const path = request.url?.toLowerCase();
-        let config: InternalAxiosRequestConfig<any> | Promise<InternalAxiosRequestConfig<any>> = request;
-        switch (path) {
-            case "/api/account/create":
-                config = transformCreateAccountRequest(config);
-                break;
-            case "/api/account/verify":
-                config = transformCreateAccountRequest(config);
-                break;
-            default:
-                break;
-        }
-        return config;
+    const path = request.url?.toLowerCase();
+    let config: InternalAxiosRequestConfig<any> | Promise<InternalAxiosRequestConfig<any>> = request;
+    switch (path) {
+        case "/api/account/create":
+            config = transformCreateAccountRequest(config);
+            break;
+        case "/api/account/verify":
+            config = transformCreateAccountRequest(config);
+            break;
+        default:
+            break;
     }
-    return request;
+    return config;
 }
 
 function listenResponse(response: AxiosResponse<any, any>): AxiosResponse<any, any> | Promise<AxiosResponse<any, any>> {
     const path=response.config.url?.toLowerCase();
+
 }
 
 export function inject(instance: AxiosInstance | null): InterceptorId {
